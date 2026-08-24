c19837a6a2d8402fb844a0dae282c364919107d0
꧁𝙾𝚗𝚕𝚢 𝙲𝚛𝚢𝚙𝚝𝚢 ?꧂ <wmax8808@gmail.com>
2026-08-24 19:24:05 +0100

    Fix undici module parsing error in webpack config
    Added webpack configuration to handle undici module parsing error.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,21 @@ const nextConfig = {
     esmExternals: 'loose',
   },
   transpilePackages: ['@firebase', 'firebase'],
+  webpack: (config, { isServer }) => {
+    // Fix for undici module parsing error
+    if (!isServer) {
+      config.module.rules.push({
+        test: /node_modules\/undici\/.*\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
