9b493abb56eec20df773966ce65e537e52464d64
꧁𝙾𝚗𝚕𝚢 𝙲𝚛𝚢𝚙𝚝𝚢 ?꧂ <wmax8808@gmail.com>
2026-08-24 23:19:05 +0100

    Simplify next.config.js by removing unused settings
    Removed transpile modules configuration and experimental settings.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-const withTM = require('next-transpile-modules')([
-  '@firebase',
-  'firebase',
-  'undici',
-]);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  experimental: {
-    esmExternals: 'loose',
-  },
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
